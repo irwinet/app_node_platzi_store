9ac7c4e7095212557c25b1fee712340e6e914429
Irwin Estrada <ext_iestrada@tottus.com.pe>
2022-10-14 18:18:45 -0500

    fix migrations

--- db/migrations/20221013153929-create-user.js
+++ db/migrations/20221013153929-create-user.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const { UserSchema, USER_TABLE } = require('./../models/user.model');
+const { DataTypes, Sequelize } = require('sequelize');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
@@ -11,7 +12,29 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-    await queryInterface.createTable(USER_TABLE, UserSchema);
+    await queryInterface.createTable(USER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'create_at',
+        defaultValue: Sequelize.NOW
+      }
+    });
   },
 
   async down (queryInterface, Sequelize) {
--- db/migrations/20221013155746-add-role.js
+++ db/migrations/20221013155746-add-role.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const { UserSchema, USER_TABLE } = require('./../models/user.model');
+const { USER_TABLE } = require('./../models/user.model');
+const { DataTypes } = require('sequelize');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
@@ -11,7 +12,11 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-    await queryInterface.addColumn(USER_TABLE, 'role', UserSchema.role);
+    await queryInterface.addColumn(USER_TABLE, 'role', {
+      allowNull: false,
+      type: DataTypes.STRING,
+      defaultValue: 'customer'
+    });
   },
 
   async down (queryInterface, Sequelize) {
