b98b63e424ba9dbc62972ec7c841bb6de658ccc4
Irwin Estrada <ext_iestrada@tottus.com.pe>
2022-10-14 18:22:07 -0500

    fix order schema

--- db/migrations/20221014162905-order.js
+++ db/migrations/20221014162905-order.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const { OrderSchema, ORDER_TABLE } = require('./../models/order.model');
+const { DataTypes, Sequelize } = require('sequelize');
+const { CUSTOMER_TABLE } = require('./customer.model');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
@@ -11,7 +13,31 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-     await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+     await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      customerId: {
+        field: 'customer_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'create_at',
+        defaultValue: Sequelize.NOW
+      }
+    });
   },
 
   async down (queryInterface, Sequelize) {
